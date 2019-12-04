943ddeee1f4ffedbb1c4c985c4199eb801b58dd2
ross <rosenlof.ross@gmail.com>
2019-12-03 18:49:48 -0700

    Initial Commit

---
+++ Project 4 Rosenlof Ross/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Project 4 Rosenlof Ross
+//
+//  Created by Ross on 12/3/19.
+//  Copyright © 2019 Ross. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
