0ce449cb090710312707efb4fbf426d872a66299
ross <rosenlof.ross@gmail.com>
2019-12-11 10:19:25 -0700

    adding text view extension, some font changes, other small styling changes

--- Project 4 Rosenlof Ross/Views/FontView.swift
+++ Project 4 Rosenlof Ross/Views/FontView.swift
@@ -11,25 +11,16 @@ import SwiftUI
 struct FontView: View {
     @State private var fontAttribute = 0
     @State private var fontSize: CGFloat = 16.0
-    var customFont: CustomFont
     @State private var customText = ""
+    @State  var isDark = false
     
+    var customFont: CustomFont
     var minFontSize: CGFloat = 12.0
     var maxFontSize: CGFloat = 72.0
     
+    
     var body: some View {
         VStack {
-            
-//                VStack(alignment: .leading) {
-//                    Text("Attributes:")
-//                        .fontWeight(.ultraLight)
-//                    Picker(selection: $fontAttribute, label: Text("Font Attribute")) {
-//                        Text("Normal").tag(0)
-//                        Text("Bold").bold().tag(1)
-//                        Text("Italic").italic().tag(2)
-//                    }.pickerStyle(SegmentedPickerStyle())
-//                }
-            
             HStack {
                 VStack(alignment: .leading) {
                     Text(customFont.full_name)
@@ -49,22 +40,90 @@ struct FontView: View {
                 .font(.custom("\(customFont.ttf_name)", size: 72))
             }
             
-            
-            TextField("Enter Your Text Here", text: $customText)
-                .font(.custom("\(customFont.ttf_name)", size: fontSize))
-//                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                .lineLimit(nil)
-            
+            HStack {
+                Text("Enter Your Text Here:")
+                    .fontWeight(.ultraLight)
+                Spacer()
+            }
+                
+            TextView(text: $customText, fontName: customFont.ttf_name, fontSize: fontSize, isDarkMode: isDark)
             
             
             Spacer()
-            }
+        }
         .padding()
         
         .navigationBarTitle(Text(customFont.full_name), displayMode: .inline)
+        .navigationBarItems(trailing:
+            Button(action: {
+                self.isDark = !self.isDark
+            }, label: {
+                Text(self.isDark ? "Light" : "Dark")
+            })
+        )
     }
         
     
+}
+
+struct TextView: UIViewRepresentable {
+    @Binding var text: String
+    var fontName: String
+    var fontSize: CGFloat
+    var isDarkMode: Bool
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> UITextView {
+
+        let myTextView = UITextView()
+        myTextView.delegate = context.coordinator
+
+        myTextView.font = UIFont(name: "\(fontName)", size: fontSize)
+        myTextView.isScrollEnabled = true
+        myTextView.isEditable = true
+        myTextView.isUserInteractionEnabled = true
+//        if (isDarkMode) {
+//            myTextView.backgroundColor = UIColor(white: 0.0, alpha: 0.97)
+//            myTextView.textColor = UIColor(hue: 0, saturation: 0, brightness: 1, alpha: 0.9)
+//        } else {
+//            myTextView.backgroundColor = UIColor(white: 0.0, alpha: 0.03)
+//            myTextView.textColor = UIColor(hue: 0, saturation: 1, brightness: 0, alpha: 0.8)
+//        }
+        
+        return myTextView
+    }
+
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+        uiView.font = UIFont(name: "\(fontName)", size: fontSize)
+        if (isDarkMode) {
+            uiView.backgroundColor = UIColor(white: 0.0, alpha: 0.97)
+            uiView.textColor = UIColor(hue: 0, saturation: 0, brightness: 1, alpha: 0.9)
+        } else {
+            uiView.backgroundColor = UIColor(white: 0.0, alpha: 0.03)
+            uiView.textColor = UIColor(hue: 0, saturation: 1, brightness: 0, alpha: 0.8)
+        }
+    }
+
+    class Coordinator : NSObject, UITextViewDelegate {
+
+        var parent: TextView
+
+        init(_ uiTextView: TextView) {
+            self.parent = uiTextView
+        }
+
+        func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+            return true
+        }
+
+        func textViewDidChange(_ textView: UITextView) {
+            self.parent.text = textView.text
+        }
+    }
 }
 
 struct FontView_Previews: PreviewProvider {
